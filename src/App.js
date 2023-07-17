945f1e20bba9d443b5226e8b4a2010cbd90842e0
oyinda-subair <oyinda.subair@gmail.com>
2023-07-17 23:15:51 +0100

    increment attribute and show class list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import { useState } from "react";
+import "./App.css";
+import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts.js";
 
 function App() {
   const [num, setNum] = useState(0);
+  const [strength, setStrength] = useState(0);
+  const [dexterity, setDexterity] = useState(0);
+  const [constitution, setConstitution] = useState(0);
+  const [intelligence, setIntelligence] = useState(0);
+  const [wisdom, setWisdom] = useState(0);
+  const [charisma, setCharisma] = useState(0);
+  const [showAttrStat, setShowAttrStat] = useState(false);
+
+  const handleShowStat = (e, name) => {
+    e.preventdefault();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,8 +25,70 @@ function App() {
         <div>
           Value:
           {num}
-          <button>+</button>
+          <button onClick={() => setNum(num + 1)}>+</button>
           <button>-</button>
+        </div>
+        <div id="grid">
+          <div id="item1">
+            <h2>Attributes</h2>
+            <div>
+              Strength:
+              {strength}
+              <button onClick={() => setStrength(strength + 1)}>+</button>
+              <button onClick={() => setStrength(strength - 1)}>-</button>
+            </div>
+            <div>
+              Dexterity:
+              {dexterity}
+              <button onClick={() => setDexterity(dexterity + 1)}>+</button>
+              <button onClick={() => setDexterity(dexterity - 1)}>-</button>
+            </div>
+            <div>
+              Constitution:
+              {constitution}
+              <button onClick={() => setConstitution(constitution + 1)}>
+                +
+              </button>
+              <button onClick={() => setConstitution(constitution - 1)}>
+                -
+              </button>
+            </div>
+            <div>
+              Intelligence:
+              {intelligence}
+              <button onClick={() => setIntelligence(intelligence + 1)}>
+                +
+              </button>
+              <button onClick={() => setIntelligence(intelligence - 1)}>
+                -
+              </button>
+            </div>
+            <div>
+              Wisdom:
+              {wisdom}
+              <button onClick={() => setWisdom(wisdom + 1)}>+</button>
+              <button onClick={() => setWisdom(wisdom - 1)}>-</button>
+            </div>
+            <div>
+              Charisma:
+              {charisma}
+              <button onClick={() => setCharisma(charisma + 1)}>+</button>
+              <button onClick={() => setCharisma(charisma - 1)}>-</button>
+            </div>
+          </div>
+          <div id="item2">
+            <h2>Classes</h2>
+            {Object.keys(CLASS_LIST).map((e, i) => {
+              return (
+                <span key={i}>
+                  {e}
+                  <br />
+                </span>
+              );
+            })}
+          </div>
+          {showAttrStat && <div id="item3"></div>}
+          <div id="item4"></div>
         </div>
       </section>
     </div>
